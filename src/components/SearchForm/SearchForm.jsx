247a273e0e861b0055303dfdc4214ce2ad5dcee9
Enital <117684114+Enital@users.noreply.github.com>
2023-06-17 16:02:01 +0300

    task 3

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
 const regions = [
   { id: 'africa', value: 'africa', name: 'Africa' },
@@ -9,13 +10,25 @@ const regions = [
   { id: 'oceania', value: 'oceania', name: 'Oceania' },
 ];
 
-export const SearchForm = () => {
+export const SearchForm = ({onSubmit}) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(query);
+    setQuery('');
+  }
+  
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
+      <Select onChange={ handleChange}  aria-label="select" name="region" required>
         <option selected disabled defaultValue="">
           Select a region and press enter
         </option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,16 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useFetchSearchCountry } from 'hooks/useFetchSearchCountry';
 
 export const CountrySearch = () => {
+  const { countries, isLoading, error, handleSubmit } = useFetchSearchCountry();
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={handleSubmit} />
+        <CountryList countries={countries}/>
       </Container>
     </Section>
   );
